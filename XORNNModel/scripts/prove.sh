d998494c9e9f7fe4088601524bef7bf663f08e38
kaku <heyingkai326@outlook.com>
2026-05-20 16:25:23 +0900

    he: add ploynomial commitment and proof&varification key generation

---
+++ XORNNModel/scripts/prove.sh
@@ -0,0 +1,5 @@
+ezkl prove \
+-w artifacts/witness.json \
+-m artifacts/model.compiled \
+-p artifacts/pk.key \
+-o artifacts/proof.json
---
+++ XORNNModel/scripts/setup.sh
@@ -0,0 +1,7 @@
+ezkl get-srs \
+--settings-path artifacts/setting.json 
+
+ezkl setup \
+--compiled-circuit artifacts/model.compiled \
+--vk-path artifacts/vk.key \
+--pk-path artifacts/pk.key
