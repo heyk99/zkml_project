0f136183753aa2f62f5175a61f9f8ce051a645cb
kaku <heyingkai326@outlook.com>
2026-05-20 16:44:07 +0900

    he: add verification

--- XORNNModel/scripts/prove.sh
+++ XORNNModel/scripts/prove.sh
@@ -1,6 +1,5 @@
 ezkl prove \
 --witness artifacts/witness.json \
 --compiled-circuit artifacts/model.compiled \
---pk-path artifacts/pk.key 
-
-mv proof.json artifacts/
+--pk-path artifacts/pk.key \
+--proof-path artifacts/proof.json
---
+++ XORNNModel/scripts/verify.sh
@@ -0,0 +1,4 @@
+ezkl verify \
+--proof-path artifacts/proof.json \
+--settings-path artifacts/setting.json \
+--vk-path artifacts/vk.key
